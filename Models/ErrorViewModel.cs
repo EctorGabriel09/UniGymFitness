dd2b40773464aa7e6671e6d1b164053a8bf5cb1d
EctorGabriel09 <teixeragabriel16@gmail.com>
2026-06-23 23:31:45 -0300

    Criando a estrutura inicial do projeto

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace UniGymFitness.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
