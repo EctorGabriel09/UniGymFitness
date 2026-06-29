46f871c6189eba1a4e715351afc70621e6f72aed
EctorGabriel09 <teixeragabriel16@gmail.com>
2026-06-28 22:06:02 -0300

    Implementando login funcional integrado ao banco de dados

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -1,17 +1,51 @@
 using Microsoft.AspNetCore.Mvc;
+using UniGymFitness.Data;
+using UniGymFitness.Models;
+using System.Linq;
 
 namespace UniGymFitness.Controllers
 {
     public class AccountController : Controller
     {
+        private readonly AppDbContext _context;
+
+        public AccountController(AppDbContext context)
+        {
+            _context = context;
+        }
+
         public IActionResult Login()
         {
             return View();
         }
 
+        [HttpPost]
+        public IActionResult Login(string Email, string Senha)
+        {
+            var usuario = _context.Usuarios
+                .FirstOrDefault(u => u.Email == Email && u.Senha == Senha);
+
+            if (usuario == null)
+            {
+                ViewBag.Erro = "E-mail ou senha inválidos.";
+                return View();
+            }
+
+            return RedirectToAction("Index", "Home");
+        }
+
         public IActionResult Cadastro()
         {
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult Cadastro(Usuario usuario)
+        {
+            _context.Usuarios.Add(usuario);
+            _context.SaveChanges();
+
+            return RedirectToAction("Login");
         }
     }
 }
